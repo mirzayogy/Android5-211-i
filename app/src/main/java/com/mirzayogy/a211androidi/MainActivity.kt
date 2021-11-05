376bd49b9c95ed3d83de980860ecc76aa44ae0e7
mirzayogy <mirza.yogy@gmail.com>
2021-11-05 15:05:50 +0800

    Toast

--- app/src/main/java/com/mirzayogy/a211androidi/MainActivity.kt
+++ app/src/main/java/com/mirzayogy/a211androidi/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +18,7 @@ class MainActivity : AppCompatActivity() {
         val emailEditText = findViewById<EditText>(R.id.emailEditText)
 
         simpanButton.setOnClickListener {
-            namaEditText.setText("")
-            emailEditText.setText("")
+            Toast.makeText(this,"Hai ",Toast.LENGTH_SHORT).show()
         }
 
         batalButton.setOnClickListener {
