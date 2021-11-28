f3f2e31496acffcbd25c1d7fbe8dea349915ad7d
mirzayogy <mirza.yogy@gmail.com>
2021-11-28 14:38:13 +0800

    login, sharedpreference, logout

--- app/src/main/java/com/mirzayogy/a211androidi/MainActivity.kt
+++ app/src/main/java/com/mirzayogy/a211androidi/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         val simpanButton = findViewById<Button>(R.id.simpanButton)
         val batalButton = findViewById<Button>(R.id.batalButton)
+        val logoutButton = findViewById<Button>(R.id.logoutButton)
 
         val namaEditText = findViewById<EditText>(R.id.namaEditText)
         val emailEditText = findViewById<EditText>(R.id.emailEditText)
@@ -37,6 +38,20 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("tanggalLahir", tanggalLahir)
             intent.putExtra("jumlahMatakuliah", jumlahMatakuliah)
             startActivity(intent)
+        }
+
+        logoutButton.setOnClickListener {
+
+            val sharedPreferences = this.getSharedPreferences("MY_SP", MODE_PRIVATE)
+            with(sharedPreferences.edit()) {
+                putString("username", "")
+                putString("password", "")
+                apply()
+            }
+
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+
         }
 
         batalButton.setOnClickListener {
